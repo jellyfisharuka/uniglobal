e9d4f1d42a7b25efff0502396ebe79ea808bb4d7
jellyfisharuka <arukeulen@gmail.com>
2025-02-11 00:23:39 +0500

    password auth added

---
+++ internal/auth/password.go
@@ -0,0 +1,15 @@
+package auth
+
+import "golang.org/x/crypto/bcrypt"
+
+func hashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+func CheckPassword(hashedPassword string, plainPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainPassword))
+	return err == nil
+}
