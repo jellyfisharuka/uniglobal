0b8b5d931e7941676ba39fd29ef338b6d741cf84
xvrwedhk <hhakimov140@gmail.com>
2025-04-27 19:43:51 +0500

    feat: update queries.go

--- internal/models/queries.go
+++ internal/models/queries.go
@@ -7,23 +7,22 @@ type Query struct {
 	Answer   string
 	User     User
 }
-
 type Chat struct {
-	ID       int       `gorm:"primaryKey"`
-	UserID   int       `gorm:"not null"`
-	Messages []Message `gorm:"foreignKey:ChatID"`
+	ID       int       `gorm:"primaryKey" json:"id"`
+	UserID   int       `gorm:"not null" json:"user_id"`
+	Messages []Message `gorm:"foreignKey:ChatID" json:"messages"`
 }
 
 type Message struct {
-	ID        uint      `gorm:"primaryKey"`
-	ChatID    uint      `gorm:"not null"`
-	Prompt    string    `gorm:"type:text;not null"`
-	Answer    string    `gorm:"type:text;not null"`
-	SenderID  uint      `gorm:"not null"` 
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	IsLiked bool `gorm:"is_liked"` //liked gpt answer for user favs 
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	ChatID    uint      `gorm:"not null" json:"chat_id"`
+	Prompt    string    `gorm:"type:text;not null" json:"prompt"`
+	Answer    string    `gorm:"type:text;not null" json:"answer"`
+	SenderID  uint      `gorm:"not null" json:"sender_id"` // The ID of the user sending the message
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+	IsLiked   bool      `gorm:"is_liked"`
 }
 
 type MessageSwagger struct {
-    Prompt  string `json:"prompt"`  // The message content (input from user)
+	Prompt string `json:"prompt"` // The message content (input from user)
 }
